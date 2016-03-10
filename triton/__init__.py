16645ebb5cd01572a5310023cbb1d247d1b4dcbb
Dave Auerbach <david@postmates.com>
2016-03-09 18:50:10 -0800

    version bump

--- triton/__init__.py
+++ triton/__init__.py
@@ -7,7 +7,7 @@ triton
 """
 
 __title__ = 'triton'
-__version__ = '0.0.9'
+__version__ = '0.0.10'
 __description__ = 'Triton - Kinesis Data Pipeline'
 __url__ = 'https://github.com/postmates/triton-python'
 __build__ = 0
