1f5049fb60d6a1ea26c53348802d65d80ef096b3
Jong Park <jong.park@postmates.com>
2019-11-01 10:21:41 -0700

    remove unicode

--- triton/encoding.py
+++ triton/encoding.py
@@ -22,20 +22,10 @@ def msgpack_encode_default(obj):
     except Exception:
         raise TypeError("Unknown type: %r" % (obj,))
 
+
 def unicode_to_ascii_str(text):
-    #if unicode, escape out multibyte characters
-    if isinstance(text, unicode):
-        return text.encode('utf-8')
-    else:
-        #need to str here because this function could be fed something
-        #that's not unicode but needs to be an ascii string (e.g. an int)
-        return str(text)
+    return str(text)
+
 
 def ascii_to_unicode_str(text):
-    #if ascii/escaped unicode, decode to utf-8
-    if isinstance(text, str):
-        return text.decode('utf-8')
-    else:
-        #need to unicode here because this function could be fed something
-        #that's not an ascii str but needs to be a unicode string (e.g. an int)
-        return unicode(text)
+    return str(text)
